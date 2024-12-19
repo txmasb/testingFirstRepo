58a1f87a1278eae471440342f569ff688f94ea8a
Tomás Boo Becerra <tomasboob@gmail.com>
2024-12-19 18:27:11 +0100

    Initial Commit

---
+++ TwitterButtonExper/TwitterButtonExperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterButtonExperApp.swift
+//  TwitterButtonExper
+//
+//  Created by Tomás Boo Becerra on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TwitterButtonExperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
